1def8f651f08ee9890d3440ed4803dfec4c8eaa3
Jose Caycho <jose.caycho.v@uni.pe>
2024-06-14 14:02:37 -0400

    connect db and routes get() and get(id)

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -5,7 +5,7 @@ const MongoClient = require('mongodb').MongoClient;
 // MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
 
-let dbInstance = null;
+let dbInstance = null;  
 const dbName = "giftdb";
 
 async function connectToDatabase() {
@@ -16,13 +16,13 @@ async function connectToDatabase() {
     const client = new MongoClient(url);      
 
     // Task 1: Connect to MongoDB
-    // {{insert code}}
+    await client.connect();
 
     // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+    const dbInstance = client.db(dbName);
 
     // Task 3: Return database instance
-    // {{insert code}}
+    return dbInstance
 }
 
 module.exports = connectToDatabase;
