8445e47d41b2b238bbe32feda071c154ef47d80d
Jose Gabriel <jose.caycho.v@uni.pe>
2024-06-15 15:41:46 -0400

    frontend Details Page

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -6,18 +6,21 @@ import RegisterPage from './components/RegisterPage/RegisterPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import DetailsPage from './components/DetailsPage/DetailsPage';
+
 function App() {
-  const navigate = useNavigate();
-  return (
+    const navigate = useNavigate();
+    return (
         <>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-          <Route path="/app/login" element={<LoginPage/>} />
-          <Route path="/app/register" element={<RegisterPage />} />
-        </Routes>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/app" element={<MainPage />} />
+                <Route path="/app/login" element={<LoginPage />} />
+                <Route path="/app/register" element={<RegisterPage />} />
+                <Route path="/app/product/:productId" element={<DetailsPage />} />
+            </Routes>
         </>
-  );
+    );
 }
 export default App;
